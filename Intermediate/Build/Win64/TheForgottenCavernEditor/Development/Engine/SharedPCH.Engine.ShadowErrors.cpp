77f6e135a120d9423dc44bb0bf8b83ca820edc36
Galen Turoci <50220824+gdturoci@users.noreply.github.com>
2020-10-20 11:05:56 -0700

    Add files via upload

---
+++ Intermediate/Build/Win64/TheForgottenCavernEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Programs/Epic Games/UE_4.25/Projects/TheForgottenCavern/Intermediate/Build/Win64/TheForgottenCavernEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/TheForgottenCavern/TheForgottenCavernGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheForgottenCavernGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEFORGOTTENCAVERN_API ATheForgottenCavernGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
