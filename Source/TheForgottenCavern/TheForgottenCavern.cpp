114e242ae480b1c4a576283b8285f62b2a569879
Galen Turoci <50220824+gdturoci@users.noreply.github.com>
2020-10-25 20:35:27 -0700

    Delete TheForgottenCavern.cpp

--- Source/TheForgottenCavern/TheForgottenCavern.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TheForgottenCavern.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheForgottenCavern, "TheForgottenCavern" );
